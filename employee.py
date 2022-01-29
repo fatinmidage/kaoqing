9883f51843788d153438f31eacd749d8bc9e1188
fatinmidage <fg1048596@live.com>
2022-01-29 23:02:46 +0800

    更新员工考勤信息

--- employee.py
+++ employee.py
@@ -1,12 +1,26 @@
 class Employee:
     def __init__(self, name):
-      self.name = name
-      self.yingchu_days = 0
-      self.actual_days = 0
-      self.personal_leave_days = 0
-      self.holidays = 0
-      self.delay_count = 0
-      self.buka_count = 0
-      self.quanqing = True
-      self.six_day_mode = 0
+      self.__name = name
+      self.__yingchu_days = 0
+      self.__actual_days = 0
+      self.__personal_leave_days = 0
+      # self.__holidays = 0
+      # self.__delay_count = 0
+      # self.__buka_count = 0
+      self.__quanqing = True
+      self.__six_day_mode = False
+    
+    def get_name(self):
+      return self.__name
 
+    def set_six_workdays_mode(self):
+      self.__six_day_mode = True
+    
+    def get_workdays_mode(self):
+      return self.__six_day_mode
+
+    def set_yingchu_days(self,days):
+      self.__yingchu_days = days
+
+    def add_actual_workdays(self,):
+      self.__actual_days +=1
--- kaoqing.py
+++ kaoqing.py
@@ -1,4 +1,5 @@
 # from email.mime import image
+from math import ceil
 from openpyxl import load_workbook
 from chinese_calendar import holidays, is_workday, is_holiday, is_in_lieu
 import calendar
@@ -87,7 +88,6 @@ def get_workday_info(year,month):
 def update_basic_info(ws, year, month, info):
     ws['b1'] = month
     ws['b2'] = month if month<12 else 1
-    print(ws['b2'].value)
     ws['b3'] = year
     ws['b5'] = info['workday_count']
     ws['b6'] = info['holiday_count']
@@ -103,10 +103,9 @@ def get_filename(name_prefix):
             filename += each
     return filename
 
-    
 def get_employees():
     filename = get_filename('广丰惠州_月度汇总')
-    wb = load_workbook(filename,data_only=True)
+    wb = load_workbook(filename)
     ws = wb['月度汇总']
     iter_employees = ws.iter_cols(max_col=1,min_row=5,max_row=ws.max_row,values_only=True)
     employees_list = {}
@@ -115,37 +114,53 @@ def get_employees():
             if i != '陈江':
                 employees_list[i] = Employee(i)
             if i == '刘光华' or i == '林锦萍' or i == '张辉':
-                employees_list[i].six_day_moda=True
+                employees_list[i].set_six_workdays_mode()
     return employees_list
+
+def get_kaoqing_details(filename):
+    wb = load_workbook(filename)
+    ws = wb.active
+    title = []
+    for row in ws.iter_rows(min_row=3, max_row=3, min_col=1, max_col=37,values_only=True):
+        for cell in row:
+            title.append(cell)
+    ws.delete_rows(1,4)
+    ws.delete_cols(38,ws.max_column-37)
+    return pd.DataFrame(ws.values, columns=title)
+
+def update_employees_kaoqing_info(em, workday_info, dataframe):
+    if em.get_workdays_mode() == True:
+        em.set_yingchu_days(workday_info['six_workdays'])
+    else:
+        em.set_yingchu_days(workday_info['workday_count'])
+    penson = dataframe.loc[dataframe['姓名']==em.get_name()]
+    for row in penson.itertuples():
+        if row[9] != '休息' and row[9] != '':
+            if row[23] == '1':
+                em.add_actual_workdays()
+            elif row[37] is not None:
+                if int(row[37]) >= 7:
+                    em.add_actual_workdays()
+    pass
 
 def update_hengke_info(ws, year, month, employees_list):
     ws['a2'] = '惠州恒科房地产开发有限公司%s年%s月份员工考勤确认统计表' % (year,month)
     ws['p3'] = '制表日期:%s-%s-1' % (year,month)
 
-def update_employees_kaoqing_info(em, workday_info):
-    if em.six_day_mode == True:
-        em.yingchu_days = workday_info['six_workdays']
-    else:
-        em.yingchu_days = workday_info['workday_count']
-    
-    filename = get_filename('广丰惠州_每日统计')
-    wb = load_workbook(filename)
-    ws = wb.active
-    
-
-
-
 def main():
-    year = 2022
-    month = 1
-    workday_info = get_workday_info(year,month)
-    employees_list = get_employees()
-    update_employees_kaoqing_info(employees_list['吴英恒'], workday_info)
-
+    year = 2021
+    month = 12
     wb = load_workbook('.//考勤统计表//考勤确认表.xlsx',data_only=True)
 
     # 更新基础信息表
+    workday_info = get_workday_info(year,month)
     update_basic_info(wb['基础信息表'], year, month, workday_info)
+
+    # 更新员工考勤信息
+    employees_list = get_employees()
+    df = get_kaoqing_details(get_filename('广丰惠州_每日统计'))
+    for each in employees_list.values():
+        update_employees_kaoqing_info(each, workday_info, df)
 
     # 更新恒科表单
     update_hengke_info(wb['恒科'], year, month, employees_list)
